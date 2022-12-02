2d452c0461331b2764e559876cabc0a6280f80c6
WeiYC <166970595@qq.com>
2022-12-02 13:07:20 +0800

    hot-master commit001

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -5,6 +5,7 @@ package com.atguigu;
  * @Since version-1.0
  * @Description
  */
+@SuppressWarnings({"all"})
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
