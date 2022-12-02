444ac0348ec99e13ad7fa6b9eac50821354ab15e
WeiYC <166970595@qq.com>
2022-12-02 12:30:24 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/**
+ * @Author wyc
+ * @Since version-1.0
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+
+}
